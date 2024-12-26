8cf4e031586a36fa386169099c26da01a97e1215
nitesh-github <53398814+nitesh-github@users.noreply.github.com>
2024-12-26 20:04:28 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/', router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, MERN with TypeScript! 2');
+  res.send('Hello, MERN with TypeScript! 3');
 });
 
 // Connect to MongoDB using Mongoose
