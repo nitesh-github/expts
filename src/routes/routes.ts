e61283fd07912ef765b90668f0b8b6252997b5b2
Nitesh Singh <nitesh.myemail@gmail.com>
2025-02-11 19:06:57 +0530

    commented

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,5 +3,6 @@ import authRoutes from './authRoutes';
 //import basicAuthRoutes from './basicAuthRoutes';
 const router = Router();
 router.use("/api", authRoutes);
+//commented 
 //router.use("/api", basicAuthRoutes);
 export default router;
